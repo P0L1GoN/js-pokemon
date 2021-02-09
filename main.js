6032072676c739e39739fa58f509307e527cf842
P0L1GoN <P0L1GoN@users.github.com>
2021-02-09 21:37:41 +0200

    added logs(Pr8)

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 let kick=document.querySelector("#btn-kick")
+const logs = [
+    'Pikachu вспомнил что-то важное, но неожиданно Charmander, не помня себя от испуга, ударил в предплечье врага.',
+    'Pikachu поперхнулся, и за это Charmander с испугу приложил прямой удар коленом в лоб врага.',
+    'Pikachu забылся, но в это время наглый Charmander, приняв волевое решение, неслышно подойдя сзади, ударил.',
+    'Pikachu пришел в себя, но неожиданно Charmander случайно нанес мощнейший удар.',
+    'Pikachu поперхнулся, но в это время Charmander нехотя раздробил кулаком \<вырезанно цензурой\> противника.',
+    'Pikachu удивился, а Charmander пошатнувшись влепил подлый удар.',
+    'Pikachu высморкался, но неожиданно Charmander провел дробящий удар.',
+    'Pikachu пошатнулся, и внезапно наглый Charmander беспричинно ударил в ногу противника',
+    'Pikachu расстроился, как вдруг, неожиданно Charmander случайно влепил стопой в живот соперника.',
+    'Pikachu пытался что-то сказать, но вдруг, неожиданно Charmander со скуки, разбил бровь сопернику.'
+];
+let logFight=[]
+let loger=[]
 let pokemon={
     defaultHP: 100,
     damageHP: 100,
@@ -26,6 +40,7 @@ kick.onclick=()=>{
     Pikachu.damageHP-=Charmander.damage
     Charmander.damageHP-=Pikachu.damage
     healthRender()
+    logsRender(Pikachu.damage)
     if(Pikachu.damageHP<=0 || Charmander.damageHP<=0){
         if(Pikachu.damageHP<=0)
             alert("Чармандер выиграл")
@@ -38,8 +53,16 @@ kick.onclick=()=>{
         Pikachu.damage=getRandom(10,50)
         Charmander.damage=getRandom(10,50)
         healthRender()
+        logFight=[]
+        let logR=document.querySelector('#logs')
+        logR.innerHTML=''
     }
-}   
+}
+function logsRender(damage){
+    let logR=document.querySelector('#logs')
+    logFight.push(`<p>${logs[Math.floor(Math.random() * logs.length)]}, нанеся ${damage} урона</p>`)
+    logR.innerHTML=logFight.reverse().join('')
+}
 function getRandom(min, max){
     min = Math.ceil(min)
     max = Math.floor(max)
